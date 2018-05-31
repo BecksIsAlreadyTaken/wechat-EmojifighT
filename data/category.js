7b8bd5caf1d05469670fa3418d9fc97bc4edef5f
beck <503646976@qq.com>
2018-05-31 13:54:54 +0800

    Init nodejs.

---
+++ data/category.js
@@ -0,0 +1,44 @@
+Items = [
+    {
+        "tplid": 1,
+        "contents": "好啊##$@?$?@$##就算你是一流工程师##$@?$?@$##就算你出报告再完美##$@?$?@$##我叫你改报告你就要改##$@?$?@$##毕竟我是客户##$@?$?@$##客户了不起啊##$@?$?@$##sorry 客户真的了不起##$@?$?@$##以后叫他天天改报告##$@?$?@$##天天改 天天改",
+        "gifDemoUrl": "http://www.moodiary.com:3000/demo/gif/1.gif",
+        "gifType": 1,
+        "name": "sorry为所欲为（来源：TVB电视剧《读心神探》）",
+        "thumbnail": "http://www.moodiary.com:3000/demo/thumbnail/1.jpg"
+    },
+    {
+        "tplid": 2,
+        "contents": "我就是饿死##$@?$?@$##死外边 从这跳下去##$@?$?@$##也不会吃你们一点东西##$@?$?@$##真香",
+        "gifDemoUrl": "http://www.moodiary.com:3000/demo/gif/2.gif",
+        "gifType": 2,
+        "name": "我就是饿死（来源：《变形记》第八季之《远山的抉择》）",
+        "thumbnail": "http://www.moodiary.com:3000/demo/thumbnail/2.jpg"
+    },
+    {
+        "tplid": 3,
+        "contents": "没有钱啊 肯定要做的啊##$@?$?@$##不做的话没有钱用##$@?$?@$##那你不会去打工啊##$@?$?@$##有手有脚的##$@?$?@$##打工是不可能打工的##$@?$?@$##这辈子不可能打工的",
+        "gifDemoUrl": "http://www.moodiary.com:3000/demo/gif/3.gif",
+        "gifType": 3,
+        "name": "打工这辈子是不可能打工的（来源：窃格瓦拉 打工）",
+        "thumbnail": "http://www.moodiary.com:3000/demo/thumbnail/3.png"
+    },
+    {
+        "tplid": 4,
+        "contents": "没想到##$@?$?@$##你竟说出如此粗鄙之语",
+        "gifDemoUrl": "http://www.moodiary.com:3000/demo/gif/4.gif",
+        "gifType": 4,
+        "name": "你竟说出如此粗鄙之语（来源：诸葛孔明《三国演义》）",
+        "thumbnail": "http://www.moodiary.com:3000/demo/thumbnail/4.png"
+    },
+    {
+        "tplid": 5,
+        "contents": "金坷垃好处都有啥##$@?$?@$##谁说对了就给他##$@?$?@$##肥料掺了金坷垃##$@?$?@$##不流失 不蒸发 零浪费##$@?$?@$##肥料掺了金坷垃##$@?$?@$##能吸收两米下的氮磷钾",
+        "gifDemoUrl": "http://www.moodiary.com:3000/demo/gif/5.gif",
+        "gifType": 5,
+        "name": "金坷垃好处都有啥 谁说对了都给他（来源：金坷垃广告）",
+        "thumbnail": "http://www.moodiary.com:3000/demo/thumbnail/5.png"
+    }
+];
+
+exports.CATEGRORY = Items;
---
+++ data/template.js
@@ -0,0 +1,370 @@
+exports.templates = [
+    // sorry
+    {
+        id: 1,
+        name: "sorry为所欲为",
+        hash: "9564a00ec44567367a07bee43868c420a8c48e60",
+        template: [
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "好啊",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,1.18,1.56)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "就算你是一流工程师",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,3.18,4.43)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "就算你出报告再完美",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,5.31,7.43)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "我叫你改报告你就要改",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,7.56,9.93)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "毕竟我是客户",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,10.06,11.56)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "客户了不起啊",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,11.93,13.06)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "sorry 客户真的了不起",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,13.81,16.31)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "以后叫他天天改报告",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,18.06,19.56)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "天天改 天天改",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,19.59,21.60)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            }
+        ]
+    },
+
+    // wangjinze
+    {
+        id: 2,
+        name: "我就是饿死",
+        hash: "7d9e4017feed1e5ae9e114c95b57f59b051f1256",
+        template: [
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "我就是饿死",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,0,1.04)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "死外边 从这跳下去",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,1.46,2.90)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "也不会吃你们一点东西",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,3.09,4.33)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "真香",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,4.59,5.93)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            }
+        ]
+    },
+
+    // 窃格瓦拉 
+    {
+        id: 3,
+        name: "窃格瓦拉 打工",
+        hash: "a426cb71a9bc130b878a178aab8273983abe001a",
+        template: [
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "没有钱啊 肯定要做的啊",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,0,1.80)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "不做的话没有钱用",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,1.88,3.76)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "那你不会去打工啊",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,3.81,4.66)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "有手有脚的",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,4.66,5.90)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "打工是不可能打工的",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,6.02,8.42)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "这辈子不可能打工的",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,8.42,10.75)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            }
+        ]
+    },
+
+    // 诸葛孔明
+    {
+        id: 4,
+        name: "竟说出如此卑鄙之语",
+        hash: "c4f50699b9d7d96c5fdb4701efc759cd02e3f0b6",
+        template: [
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "没想到",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,0.81,1.81)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "你竟说出如此粗鄙之语",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,2.31,5.42)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            }
+        ]
+    },
+
+    // 金坷垃
+    {
+        id: 5,
+        name: "金坷垃好处都有啥 谁说对了都给他",
+        hash: "5f13a58aa24c8ddf7bfbaa1546e30c1ba8606cb6",
+        template: [
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "金坷垃好处都有啥",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,0,2.56)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "谁说对了就给他",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,2.56,4.31)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "肥料掺了金坷垃",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,2.56,5.56)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "不流失 不蒸发 零浪费",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,5.56,8.68)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "肥料掺了金坷垃",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,9.81,11.93)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            },
+            {
+                filter: "drawtext",
+                options: {
+                    "text": "能吸收两米下的氮磷钾",
+                    "x": "(w-text_w)/2",
+                    "y": "h-text_h-4",
+                    "enable": "between(t,12.06,14.68)",
+                    'fontfile': 'msyhbd.ttc',
+                    'fontcolor': 'white',
+                    "fontsize": "14"
+                }
+            }
+        ]
+    },
+]
